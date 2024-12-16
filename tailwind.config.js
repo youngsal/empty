899372a7ae002dccf7944881a6bcb5932f2766d9
youngsal <waleola805@gmail.com>
2024-12-16 15:45:04 +0100

    new

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        vuejs: {
+          100: "#03fca1",
+          300: "#8f5236"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
+
